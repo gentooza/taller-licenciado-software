9e1140e2975df35bab59ebcc628861a0c66f9236
gentooza <joa.cuellar@riseup.net>
2022-03-14 12:02:20 +0100

    added copyright header to .py file

--- ejemplos/7 hola mundo con imagenes/hola.py
+++ ejemplos/7 hola mundo con imagenes/hola.py
@@ -1,4 +1,20 @@
 #!/bin/python3
+'''
+    Copyright 2022 Joaquín Cuéllar.
+    
+    This file is part of Hola Mundo Especial.
+
+    Hola Mundo Especial is free software: you can redistribute it and/or modify it 
+    under the terms of the GNU General Public License as published by the Free Software Foundation, 
+    either version 3 of the License, or (at your option) any later version.
+
+    Hola Mundo Especial is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Hola Mundo Especial. 
+    If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import tkinter as tk
 from PIL import ImageTk, Image
